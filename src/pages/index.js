c60322741f8322db5a5b2025096e348dec884a81
unknown <bdoss1@gmail.com>
2021-08-10 16:58:11 -0500

    added title and desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="KOTS Dallas Information" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -19,10 +19,7 @@ const IndexPage = () => (
       alt="Kingz of the South"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      
-    </p>
+    
   </Layout>
 )
 
