c1d5854a3d8361576f3660b6c1b2d1f487ff2777
unknown <bdoss1@gmail.com>
2021-08-10 16:13:50 -0500

    added kots logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,16 +12,16 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../kingz_logo_small.png"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="Kingz of the South"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      
     </p>
   </Layout>
 )
